ae56c4423ec57df0193174138fbffdaf3ef18291
Phildo <phildo211@gmail.com>
2019-02-01 12:34:32 -0600

    actually correct level skip

--- src/scenes/gameplayScene.js
+++ src/scenes/gameplayScene.js
@@ -1470,8 +1470,6 @@ var GamePlayScene = function(game, stage)
     l.y_min = 0;
     for(var j = 0; j < 53; j++)
       l.context_imgs.push(GenImg("assets/context/"+i+"-"+j+".png"));
-    for(var j = 0; j < 1; j++)
-      l.system_imgs.push(GenImg("assets/system/"+(i+2)+"-"+j+".png"));
     l.pano = 0;
     l.pano_st = 0;
     l.pano_et = 0.05;
@@ -1509,6 +1507,8 @@ var GamePlayScene = function(game, stage)
     l.y_min = floor(l.b_correct_total/10)*10;
     for(var j = 0; j < 0; j++)
       l.context_imgs.push(GenImg("assets/context/"+i+"-"+j+".png"));
+    for(var j = 0; j < 1; j++)
+      l.system_imgs.push(GenImg("assets/system/"+(i+2)+"-"+j+".png"));
     l.pano = 0;
     l.pano_st = 0;
     l.pano_et = 0.05;
@@ -1517,7 +1517,7 @@ var GamePlayScene = function(game, stage)
     l.perma_zoom = 0;
     l.skip_axis = 1;
     l.skip_labels = 1;
-    l.skip_system = 1;
+    l.skip_system = 0;
     l.skip_night = 0;
     l.push_work = 1;
     l.special = 0;
